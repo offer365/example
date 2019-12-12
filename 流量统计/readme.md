d9123e39f3291bffbf398146ed853f045f98a83c
offer365 <332000640@qq.com>
2019-12-12 18:38:39 +0800

    move incomplete example

--- 流量统计/readme.md
+++
@@ -1,7 +0,0 @@
-# 注意
-linux  
-安装
-
-yum -y install libpcap
-
-yum -y install libpcap-devel
--- 设计模式/简单工厂模式/simple_factory/simple_test.go
+++
@@ -1,8 +0,0 @@
-package simple_factory
-
-import "testing"
-
-func TestNewAPI(t *testing.T) {
-	NewAPI(1).Say("tom")
-	NewAPI(2).Say("tom")
-}
